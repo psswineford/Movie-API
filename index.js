b33f1ef4df34cceb163c9e23c14e8dabea6d0e7c
Shawn Swineford <psswineford@gmail.com>
2021-09-15 14:53:11 -0700

    Task 2-4
    - moved the documentation.html file to public folder
    -removed index.html and log files.
    
    updated index.js to include middleware and express

---
+++ index.js
@@ -0,0 +1,51 @@
+const { response } = require('express');
+const express = require('express'),
+    morgan = require('morgan');
+
+const app = express();
+
+// serve files with express static 
+app.use(express.static('public'));
+
+// Log all requests utlizing Morgan
+app.use(morgan('common'));
+
+//error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+//create array of information on top movies
+let topMovies = [
+    {
+        title: 'Star Wars',
+        director: 'George Lucas'
+    },
+    {
+        title: 'The Crow',
+        director: 'Alex Proyas'
+    },
+    {
+        title: 'The Matrix',
+        director: 'The Wachoskis'
+    },
+    {
+        title: 'Lord of the Rings',
+        director: 'Peter Jackson'
+    }
+];
+
+//GET requests
+app.get('/', (req,res) => {
+    res.send('Welcome to my Movie List!');    
+});
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+// listen for requests
+app.listen(8080, () => {
+    console.log('Your app  is listening on port 8080.');
+});
