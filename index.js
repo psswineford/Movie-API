54ec44168d1f689f9878acab7ea2aa1209fa3b51
Shawn Swineford <psswineford@gmail.com>
2021-10-04 11:20:45 -0700

    Set up heroku commands

--- index.js
+++ index.js
@@ -235,6 +235,7 @@ app.delete('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { se
 });
 
 // listen for requests
-app.listen(8080, () => {
-    console.log('Your app  is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
